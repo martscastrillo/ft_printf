472468c05293c55c0b346a06f7b83b166bd8dacf
martscastrillo <mart.castrillo@gmail.com>
2023-05-22 14:21:27 +0200

    add .h file

--- ft_printf.h
+++ ft_printf.h
@@ -1,29 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: martcast <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/25 20:38:51 by martcast          #+#    #+#             */
-/*   Updated: 2023/04/25 20:38:54 by martcast         ###   ########.fr       */
+/*   Created: 2023/05/22 14:21:06 by martcast          #+#    #+#             */
+/*   Updated: 2023/05/22 14:21:09 by martcast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <unistd.h>
 
-int ft_printf(char const *, ...){
-    va_list valist;
-    va_start(valist);
-
-   
-   va_end(valist);
+int	ft_printf(char const *str, ...);
 
 
-}
-
-int main (void){
-    char str1 [] = "Hola don pepito\n";
-    ft_printf("%s", str1); 
-    printf("%s", str1);
-}
+#endif
