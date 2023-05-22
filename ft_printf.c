e2ed1168d08fe661e75d153432a45bb53d53fcdb
martscastrillo <mart.castrillo@gmail.com>
2023-05-22 14:48:41 +0200

    add file

--- ft_printf.c
+++ ft_printf.c
@@ -11,19 +11,60 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
 
-int ft_printf(char const *, ...){
-    va_list valist;
-    va_start(valist);
+int ft_printf(char const *str, ...){
+   int		i;
+	int		len;
+	va_list	args;
 
-   
-   va_end(valist);
+	i = 0;
+	len = 0;
+	va_start(args, str);
+	while (str[i] != '\0')
+	{
+		/* if (str[i] == '%')
+		{
+			i++;
+			len += ft_formats(args, str[i]);
+		}
+		else */
+			len += ft_putchar(str[i]);
+			i++;
+	}
+	va_end(args);
+	return (len);
 
 
 }
 
+#include <stdio.h>
+
 int main (void){
-    char str1 [] = "Hola don pepito\n";
-    ft_printf("%s", str1); 
-    printf("%s", str1);
+   
+    char c;
+	char *s;
+	int nb;
+	unsigned int un;
+
+	c = 'f';
+	s = "blablabla";
+	nb = -10;
+	un = 9454999;
+	
+	printf("frase: %s letra:%c\n", s, c);
+	ft_printf("frase: %s letra:%c\n", s, c);
+	printf("puntero:%p\n", &s);
+	ft_printf("puntero:%p\n", &s);
+	printf("nb = %d  o %i\n", nb, nb);
+	ft_printf("nb = %d  o %i\n", nb, nb);
+	printf("Sin signo: %u\n", un);
+	ft_printf("Sin signo: %u\n", un);
+	printf("Hexa: %x    %X\n", nb, nb);
+	ft_printf("Hexa: %x   %X\n", nb, nb);
+	return (0);
 }
