57c8666d924f032e39692cd7730b368422b6de8d
martscastrillo <mart.castrillo@gmail.com>
2023-06-17 11:47:26 +0200

    add files

--- ft_printf.c
+++ ft_printf.c
@@ -11,36 +11,98 @@
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-int	ft_putchar(char c)
+int ft_strlen(const char *str)
 {
-	write(1, &c, 1);
-	return (1);
+    int len = 0;
+    while (str[len] != '\0')
+        len++;
+    return len;
 }
 
-int ft_printf(char const *str, ...){
-   int		i;
-	int		len;
-	va_list	args;
-
-	i = 0;
-	len = 0;
-	va_start(args, str);
-	while (str[i] != '\0')
-	{
-		/* if (str[i] == '%')
-		{
-			i++;
-			len += ft_formats(args, str[i]);
-		}
-		else */
-			len += ft_putchar(str[i]);
-			i++;
-	}
-	va_end(args);
-	return (len);
-
-
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
 }
+
+void ft_putstr(const char *str)
+{
+    write(1, str, ft_strlen(str));
+}
+
+void ft_putnbr(int n)
+{
+    if (n < 0)
+    {
+        ft_putchar('-');
+        n = -n;
+    }
+    
+    if (n >= 10)
+        ft_putnbr(n / 10);
+    
+    ft_putchar((n % 10) + '0');
+}
+
+int ft_printf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    
+    int printed_chars = 0;
+    const char *str = format;
+    
+    while (*str != '\0')
+    {
+        if (*str == '%')
+        {
+            str++;
+            
+            // Manejar los modificadores de formato compatibles (%s y %d)
+            if (*str == 's')
+            {
+                char *s = va_arg(args, char*);
+                ft_putstr(s);
+                printed_chars += ft_strlen(s);
+            }
+            else if (*str == 'd')
+            {
+                int d = va_arg(args, int);
+                ft_putnbr(d);
+                
+                // Calcular la cantidad de dígitos impresos
+                if (d < 0)
+                    printed_chars++; // tener en cuenta el signo '-' en números negativos
+                
+                int temp = d;
+                int digit_count = 0;
+                while (temp != 0)
+                {
+                    temp /= 10;
+                    digit_count++;
+                }
+                
+                printed_chars += digit_count;
+            }
+            else
+            {
+                ft_putchar('%');
+                printed_chars++;
+            }
+        }
+        else
+        {
+            ft_putchar(*str);
+            printed_chars++;
+        }
+        
+        str++;
+    }
+    
+    va_end(args);
+    
+    return printed_chars;
+}
+
 
 #include <stdio.h>
 
@@ -57,14 +119,18 @@ int main (void){
 	un = 9454999;
 	
 	printf("frase: %s letra:%c\n", s, c);
-	ft_printf("frase: %s letra:%c\n", s, c);
-	printf("puntero:%p\n", &s);
-	ft_printf("puntero:%p\n", &s);
+	ft_printf("frase: %s letra:%c printf\n", s, c);
+	printf("puntero:%p \n", &s);
+	ft_printf("puntero:%p printf\n", &s);
 	printf("nb = %d  o %i\n", nb, nb);
-	ft_printf("nb = %d  o %i\n", nb, nb);
+	ft_printf("nb = %d  o %i printf\n", nb, nb);
 	printf("Sin signo: %u\n", un);
-	ft_printf("Sin signo: %u\n", un);
+	ft_printf("Sin signo: %u printf\n", un);
 	printf("Hexa: %x    %X\n", nb, nb);
-	ft_printf("Hexa: %x   %X\n", nb, nb);
+	ft_printf("Hexa: %x   %X printf\n", nb, nb);
+
+	 ft_printf("Hello, %s!\n", "world");
+    ft_printf("The answer is %d.\n", 42);
+    
 	return (0);
 }
