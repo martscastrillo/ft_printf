cbe1bc6b10a814271b306ec58ea1745cf9e7bede
martscastrillo <mart.castrillo@gmail.com>
2023-06-23 01:00:07 +0200

    ref

--- ft_printf.c
+++ ft_printf.c
@@ -49,8 +49,7 @@ int ft_printf(const char *format, ...)
                 void *p = va_arg(args, void*);
                 char buffer[20];
                 sprintf(buffer, "%p", p);
-                ft_putstr(buffer);
-                printed_chars += ft_strlen(buffer);
+                printed_chars +=  ft_putstr(buffer);
             }
             else if (*str == 'd' || *str == 'i')
             {
@@ -132,35 +131,4 @@ int ft_printf(const char *format, ...)
     va_end(args);
     
     return printed_chars;
-}
-#include <stdio.h>
-
-int main (void){
-   
-    char c;
-	char *s;
-	int nb;
-	unsigned int un;
-
-	c = 'f';
-	s = "blablabla";
-	nb = -10;
-	un = 9454999;
-	
-	printf("frase: %s letra:%c\n", s, c);
-	ft_printf("frase: %s letra:%c printf\n", s, c);
-
-	printf("puntero:%p \n", &s);
-	ft_printf("puntero:%p printf\n", &s);
-
-	printf("nb = %d  o %i\n", nb, nb);
-	ft_printf("nb = %d  o %i printf\n", nb, nb);
-
-	printf("Sin signo: %u\n", un);
-	ft_printf("Sin signo: %u printf\n", un);
-	
-	printf("Hexa: %x    %X\n", nb, nb);
-	ft_printf("Hexa: %x   %X printf\n", nb, nb);
-    
-	return (0);
 }
