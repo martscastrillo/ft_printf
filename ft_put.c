d7cca89539366fadb92eb82b065a099f644ff35f
martscastrillo <mart.castrillo@gmail.com>
2023-06-30 11:59:25 +0200

    add ftput file

--- ft_put.c
+++ ft_put.c
@@ -14,6 +14,14 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+size_t	ft_strlen(const char *s);
+
+int	ft_putstr(const char *str)
+{
+	return (write(1, str, ft_strlen(str)));
+}
+// write devuelve la cantidad de caracteres que imprimes
+
 int	ft_putchar(char c)
 {
 	return (write(1, &c, 1));
